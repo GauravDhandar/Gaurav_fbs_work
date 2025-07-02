6473af0e57758d9ca96862fedfc01c690f0339a1
Gaurav Dhandar <144891307+GauravDhandar@users.noreply.github.com>
2025-07-02 19:06:58 +0530

    Add files via upload

---
+++ C programming /Assignment0/Square & Cube . C.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main ()
+{
+	int num = 7;
+	int square, cube;
+	
+	square = num * num;
+	cube = num * num * num;
+	
+	printf(" number = %d\n",num);
+	printf(" square = %d\n",square);
+	printf(" cube  = %d\n",cube);
+
+}
