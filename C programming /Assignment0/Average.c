25aba6607d7159b27d5e55e854bf52ce196e377a
Gaurav Dhandar <144891307+GauravDhandar@users.noreply.github.com>
2025-07-02 19:10:53 +0530

    Add files via upload

---
+++ C programming /Assignment0/Average.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main ()
+{
+  int num1=17;
+  int num2=67;
+  int num3=30;
+  int num4=20;
+  int num5=41;
+   
+   float average;
+   average  = (num1+num2+num3+num4+num5)/5.0;
+   printf("the average of %d,%d,%d,%d and %d is %.2f\n",
+   num1, num2, num3, num4, num5,average);
+}
+   
+   
