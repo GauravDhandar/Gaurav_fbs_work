f33557c3ec80d6c40377f37ee59bfcb21c214a80
Gaurav Dhandar <144891307+GauravDhandar@users.noreply.github.com>
2025-07-02 19:40:01 +0530

    Add files via upload

---
+++ C programming /Assignment0/5 subject.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+    int subject1, subject2, subject3, subject4, subject5;
+    int total;
+    float percentage;
+    
+    printf("Enter marks for five subjects (out of 100):\n");
+    scanf("%d %d %d %d %d", &subject1, &subject2, &subject3, &subject4, &
