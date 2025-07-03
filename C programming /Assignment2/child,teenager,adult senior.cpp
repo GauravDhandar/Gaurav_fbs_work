2e43dda7ddef684b867fd924ee3021571a831e64
Gaurav Dhandar <144891307+GauravDhandar@users.noreply.github.com>
2025-07-03 19:35:32 +0530

    Add files via upload

---
+++ C programming /Assignment2/child,teenager,adult senior.cpp
@@ -0,0 +1,11 @@
+
+age = int(input("Enter your age: "))
+
+if age < 12:
+    print("Child")
+elif 12 <= age <= 19:
+    print("Teenager")
+elif 20 <= age <= 59:
+    print("Adult")
+else:
+    print("Senior")
