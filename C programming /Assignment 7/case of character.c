d5eb4c0a16eba7858ae10e8cb2c79412aece5cb0
Gaurav Dhandar <144891307+GauravDhandar@users.noreply.github.com>
2025-07-18 13:51:08 +0530

    Add files via upload

--- C programming /Assignment 7/case of character.c
+++ C programming /Assignment 7/case of character.c
@@ -1,41 +1,29 @@
 #include<stdio.h>
-int checkCharCase();
+void checkCharCase(char);
 void main()
 {
-int res=checkCharCase();
-if(res==1)
-{
-	printf("The given character is lowercase ");
-}
-else if(res==2)
-{
-	printf("The given character is uppercase ");
-}
-else
-{
-	printf("Enter valid character");
-}
-
-}
-int checkCharCase()
-{
-	char ch;
+			char ch;
 	printf("Enter the character to check whether it is uppercase or lowercase \n");
 	scanf("%c",&ch);
+checkCharCase(ch);
+
+}
+void checkCharCase(char ch)
+{
+
 	if(ch>='a' && ch<='z')
 	{
-		return 1;
+		printf("The given character is lowercase ");
 	}
 	else
 	{
 		if(ch>='A' && ch<='Z')
 		{
-			return 2;
-			
+			printf("The given character is uppercase ");
 		}
 		else
 		{
-			return 0;
+			printf("Enter valid character");
 		}
 	}
 }
